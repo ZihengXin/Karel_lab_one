3592ec35efe54a5e7853a6232c77d6833e446f8b
xinzi <ziheng.xin@ssfs.org>
2019-09-26 11:51:39 -0400

    9/26 8 route robot

--- src/KarelFigureEight.java
+++ src/KarelFigureEight.java
@@ -1,3 +1,4 @@
+// 9/26 Charly. 8_route robot moving.
 import becker.robots.*;
 
 public class KarelFigureEight {
@@ -11,7 +12,47 @@ public class KarelFigureEight {
         Thing thing1 = new Thing(ssfs, 2, 3);
         Thing thing2 = new Thing(ssfs, 4, 3);
 
-        // Your code goes below here
+        /* Before I started to write the code, I was confused by the definition of "8-pattern"
+        Even though the B-shape takes fewer steps to accomplish, I am not sure if the B-route follows the instruction.
+        So I chose to write the code for a classical 8-route. It looks longer than the other routes, but at least
+        it goes like how people write the number 8*/
+
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
 
     }
 }
