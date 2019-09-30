29d6f213b8f6950fd54d19c2f3f25d95cff81093
xinzi <ziheng.xin@ssfs.org>
2019-09-30 15:19:01 -0400

    9/30 enter the value of width, height, x, and y to get the new location and parameter of the new rectangle.

--- src/RectangleFun.java
+++ src/RectangleFun.java
@@ -1,5 +1,24 @@
+// 9.30 Charly. this program should be able to calculate the parameter of a Rectangle.
+import java.awt.*;
+import java.util.Scanner;
+
 public class RectangleFun {
     public static void main(String[] args){
-        System.out.println();
+        System.out.print("Width: ");
+        Scanner input=new Scanner(System.in);
+        int width=input.nextInt();
+        System.out.print("Height: ");
+        int height=input.nextInt();
+        System.out.print("X: ");
+        int x=input.nextInt();
+        System.out.print("Y: ");
+        int y= input.nextInt();
+        Rectangle r=new Rectangle(x,y,width,height);
+        double parameter= 2*r.getWidth()+2*r.getHeight();
+        r.setLocation((int)(r.getX()-4),(int)(r.getY()+2));
+        System.out.println("Parameter = "+parameter);
+        System.out.print("New location is ("+r.getX()+","+r.getY()+")");
+
+
     }
 }
