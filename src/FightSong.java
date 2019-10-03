702f5761201a7d29dff55c0e2c0c1b70e123553c
xinzi <ziheng.xin@ssfs.org>
2019-10-03 10:13:46 -0400

    10/3 print the Fight Song without using System.out.println() in the main method.

---
+++ src/FightSong.java
@@ -0,0 +1,24 @@
+public class FightSong {
+    private static void go(){
+        System.out.println("Go, team, go! \nYou can do it.");
+    }
+    private static void spaceLine(){
+        System.out.println(" ");
+    }
+    private static void you(){
+        System.out.println("You're the best,\nIn the West.");
+    }
+    public static void main(String[] args){
+        go();
+        spaceLine();
+        go();
+        you();
+        go();
+        spaceLine();
+        go();
+        you();
+        go();
+        spaceLine();
+        go();
+    }
+}
