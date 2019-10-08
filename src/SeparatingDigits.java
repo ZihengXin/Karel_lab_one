fc0d2b1923e9de97113737c83ebfdecb6b9db3cd
xinzi <ziheng.xin@ssfs.org>
2019-10-08 12:13:59 -0400

    10/8 separating digit

--- src/SeparatingDigits.java
+++ src/SeparatingDigits.java
@@ -1,2 +1,26 @@
+// Charly. Oct 08, 2019. This program should be able to separate a four digit number in to 4 numbers and display them
+// in order.
+import java.util.Scanner;
+
 public class SeparatingDigits {
+    private static int quotient(int a){
+        a/=10;
+        return a;
+        // divide the number by 10
+    }
+    private static int remainder(int a){
+        a%=10;
+        return a;
+        // get the remainder for a
+    }
+    public static void main(String[] args){
+        System.out.print("Enter a 4 digit number: ");
+        Scanner input= new Scanner(System.in);
+        int a= input.nextInt();
+        System.out.print(SeparatingDigits.quotient(SeparatingDigits.quotient(SeparatingDigits.quotient(a)))+" ");
+        System.out.print(SeparatingDigits.remainder(SeparatingDigits.quotient(SeparatingDigits.quotient(a)))+" ");
+        System.out.print(SeparatingDigits.remainder(SeparatingDigits.quotient(a))+" ");
+        System.out.print(SeparatingDigits.remainder(a));
+
+    }
 }
