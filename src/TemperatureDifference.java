c7513e7260b4d15d925fd203b1144e88bcbbec57
xinzi <ziheng.xin@ssfs.org>
2019-10-15 15:54:03 -0400

    10/15 TemperatureDifference

---
+++ src/TemperatureDifference.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TemperatureDifference {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the first temperature: ");
+        int firstTem= input.nextInt();
+        System.out.print("Enter the second temperature: ");
+        int secondTem= input.nextInt();
+        int result= Math.abs(firstTem-secondTem);
+        System.out.print("The temperature changed "+result+" degrees.");
+    }
+}
